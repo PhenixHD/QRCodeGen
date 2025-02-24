de64b886263f3494bab9c14c012c02c3ef64094b
Phenix <eliasbiyikli09@gmail.com>
2025-02-24 19:45:13 +0100

    Add project files.

---
+++ QRCodeGen/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace QRCodeGen;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
---
+++ QRCodeGen/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static System.Net.Mime.MediaTypeNames;
+using QRCoder;
+using System.IO;
+
+namespace QRCodeGen;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void GenerateQRCode(object sender, RoutedEventArgs e) {
+
+        if (!string.IsNullOrWhiteSpace(textboxURL.Text)) {
+            using (QRCodeGenerator qrGenerator = new QRCodeGenerator())
+            using (QRCodeData qrCodeData = qrGenerator.CreateQrCode(textboxURL.Text, QRCodeGenerator.ECCLevel.Q))
+            using (PngByteQRCode qrCode = new PngByteQRCode(qrCodeData)) {
+
+                byte[] qrCodeImage = qrCode.GetGraphic(50);
+
+                BitmapImage bitmap = new BitmapImage();
+                using (var ms = new MemoryStream(qrCodeImage)) {
+                    bitmap.BeginInit();
+                    bitmap.CacheOption = BitmapCacheOption.OnLoad;
+                    bitmap.StreamSource = ms;
+                    bitmap.EndInit();
+                    bitmap.Freeze();
+                }
+
+                imageQRCode.Source = bitmap;
+            }
+        }
+    }
+}
